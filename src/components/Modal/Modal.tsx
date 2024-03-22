b671c91e070060318cd92daf5cdec8ad39f459a9
Sergey Nikolaychuk <Nikolas.ser.a@yandex.ru>
2024-03-22 09:19:13 +0200

    done

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,129 @@
+	import { Button, Col, Form, Modal } from "react-bootstrap";
+	import React, { useEffect, useState } from "react";
+	import { Formik } from "formik";
+	import * as yup from "yup";
+	import { IModalProps, NewCardState } from "./Modal.type";
+	import ReactDOM from "react-dom";
+
+	const defaultCard = {
+		title: "",
+		description: "",
+	};
+
+	export const ModalComponent: React.FC<IModalProps> = ({
+		isOpen,
+		openModalCallback,
+		handleCloseModal,
+		submitModalCallback,
+		card,
+	}) => {
+
+	// const [cardState, setCardState] = useState<NewCardState>(defaultCard);
+
+	const schema = yup.object().shape({
+		title: yup.string().required("Title is required"),
+		description: yup.string().required("Description is required"),
+	});
+
+	// useEffect(() => {
+	// 	card && setCardState(card);
+	// 	console.log(card, '------- card -------');
+		
+	// }, [card]);
+
+	const handleSubmit = (values: NewCardState) => {
+		schema
+		.validate(values)
+		.then(() => {
+			submitModalCallback(values);
+			handleCloseModal();
+		})
+		.catch((error) => {
+			console.error(error)
+		});
+	};
+
+	return ReactDOM.createPortal(
+		<Modal
+			show={isOpen}
+			onHide={openModalCallback}
+			size="sm"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+		<Modal.Header closeButton>
+			<Modal.Title id="contained-modal-title-vcenter">
+			{card.description.length > 0 ? "EDIT CARD" : "ADD NEW CARD"}
+			</Modal.Title>
+		</Modal.Header>
+		
+			<Formik
+				validationSchema={schema}
+				onSubmit={handleSubmit}
+				initialValues={card}
+  				enableReinitialize={true}
+			>
+			{({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
+				<Form noValidate onSubmit={handleSubmit}>
+					<Modal.Body>
+						<Col>
+							<Form.Group
+								as={Col}
+								md="4"
+								controlId="validationFormik101"
+								className="position-relative w-100"
+							>
+							<Form.Label>Title</Form.Label>
+							<Form.Control
+								type="text"
+								name="title"
+								value={values.title}
+								onChange={handleChange}
+								onBlur={handleBlur}
+								isValid={touched.title && !errors.title}
+								isInvalid={!!errors.title}
+								readOnly
+							/>
+							<Form.Control.Feedback tooltip>
+								Looks good!
+							</Form.Control.Feedback>
+							</Form.Group>
+							<Form.Group
+								as={Col}
+								md="4"
+								controlId="validationFormik101"
+								className="position-relative w-100"
+							>
+							<Form.Label>Description</Form.Label>
+							<Form.Control
+								type="text"
+								name="description"
+								value={values.description}
+								onChange={handleChange}
+								onBlur={handleBlur}
+								isValid={touched.description && !errors.description}
+								isInvalid={!!errors.description}
+							/>
+							<Form.Control.Feedback tooltip>
+								Looks good!
+							</Form.Control.Feedback>
+							</Form.Group>
+						</Col>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={openModalCallback}>
+							Cancel
+						</Button>
+						<Button disabled={!isValid} type="submit">
+							OK
+						</Button>
+					</Modal.Footer>
+				</Form>
+			)}
+			</Formik>
+		
+		
+		</Modal>,
+		document.body
+	);
+	};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { CardContextProvider } from './context/CardsContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CardContextProvider>
+        <App />
+      </CardContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
